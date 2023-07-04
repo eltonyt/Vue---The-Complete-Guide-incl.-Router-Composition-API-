8fc434f9878ce7c33ddbdc1c57734e72384a02f0
Elton Li <elton.li@ubenefit.net>
2023-07-03 23:42:30 -0500

    Finished Module 1 - Getting Started

--- 01-getting-started/demo-with-vue/app.js
+++ 01-getting-started/demo-with-vue/app.js
@@ -1,17 +1,17 @@
 Vue.createApp({
-	data: function() {
-		return {
-			goals: [],
-			enteredValue: ''
-		};
-	},
-	methods: {
-		addGoal() {
-			this.goals.push(this.enteredValue);
-			this.enteredValue = '';
-		}
-	}
-}).mount('#app');
+  data: function () {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
