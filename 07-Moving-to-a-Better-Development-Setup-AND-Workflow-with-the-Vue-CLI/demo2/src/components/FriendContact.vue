848b9c320fba1cc31134a2d75229c17c88aebdb2
Elton Li <elton.li@ubenefit.net>
2023-07-09 21:25:17 -0500

    Module 7 - Vue CLI New Project Finished

---
+++ 07-Moving-to-a-Better-Development-Setup-AND-Workflow-with-the-Vue-CLI/demo2/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>
+      {{ friend.name }}
+    </h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend:
+        {
+          id: "123456",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
