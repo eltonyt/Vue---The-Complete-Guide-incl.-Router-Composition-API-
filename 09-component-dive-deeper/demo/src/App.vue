52da355afd20612edb19ee20eb0c8d2dfef95ab5
Elton Li <elton.li@ubenefit.net>
2023-07-22 15:42:39 -0500

    Slots with Names & Slot Props Demo Finished

--- 09-component-dive-deeper/demo/src/App.vue
+++ 09-component-dive-deeper/demo/src/App.vue
@@ -7,27 +7,38 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>
+          {{ slotProps.item }}
+        </h2>
+        <p>
+          {{ slotProps["anotherProp"] }}
+        </p>
+        </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
-
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
-    'the-header': TheHeader,
-    'user-info': UserInfo,
-    'badge-list': BadgeList
+    "the-header": TheHeader,
+    "user-info": UserInfo,
+    "badge-list": BadgeList,
+    "course-goals": CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
---
+++ 09-component-dive-deeper/demo/src/components/CourseGoals.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal"> {{ goal }}</li>
+    <slot :item="goal" another-prop="test"></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["Finish the course", "Learn Vue"],
+    };
+  },
+};
+</script>
