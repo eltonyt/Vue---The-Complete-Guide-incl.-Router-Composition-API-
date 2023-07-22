e982e7d434333cddecf559e39c5e2ff676a7316c
Elton Li <elton.li@ubenefit.net>
2023-07-22 17:03:17 -0500

    main page - content onlyh

---
+++ 10-Course-Project-A-to-Z/a-z/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <!-- TITLE -->
+        <h3>{{ title }}</h3>
+        <!-- DELETE BUTTON -->
+        <button>Delete</button>
+      </header>
+    </div>
+    <!-- DESCRIPTION -->
+    <p>{{ description }}</p>
+    <!-- LINK -->
+    <nav>
+      <a v-bind:href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    link: {
+      type: String,
+    },
+  },
+};
+</script>
