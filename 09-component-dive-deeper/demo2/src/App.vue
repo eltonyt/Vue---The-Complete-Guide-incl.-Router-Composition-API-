54e7da431d8781acd8504fe94677a5a08874b646
Elton Li <elton.li@ubenefit.net>
2023-07-22 16:27:02 -0500

    Keep Live Tag & Teleport Tag

---
+++ 09-component-dive-deeper/demo2/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">
+      Active Goals
+    </button>
+    <button @click="setSelectedComponent('manage-goals')">
+      Manage Goals
+    </button>
+    <!-- <active-goals v-if="selectedComponenet === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponenet === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="setSelectedComponent">
+      </component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+// import TheHeader from '../../demo/src/components/TheHeader.vue';
+import TheHeader from "./components/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    "the-header": TheHeader,
+    // "user-info": UserInfo,
+    // "badge-list": BadgeList,
+    // "course-goals": CourseGoals,
+    "active-goals": ActiveGoals,
+    "manage-goals": ManageGoals,
+    TheHeader,
+  },
+  data() {
+    return {
+      selectedComponenet:"active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponenet = cmp;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ 09-component-dive-deeper/demo2/src/components/ManageGoals.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>
+    Manage Goals
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Plese enter at least a few characters...</p>
+        <button @click="confirmError">OK</button>
+      </error-alert>
+    </teleport>
+  </h2>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+        alert("Input must not be empty!");
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
