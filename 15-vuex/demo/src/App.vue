cc4d57ce9450022805191b8ea27e92db5fbcb76d
Yutong Li (Elton) <eltonyt.li@gmail.com>
2023-08-08 21:04:27 -0500

    finished demo

--- 15-vuex/demo/src/App.vue
+++ 15-vuex/demo/src/App.vue
@@ -27,7 +27,7 @@ export default {
   },
   methods: {
     addOne() {
-      this.$store.dispatch('increase', { add10: 10 });
+      this.$store.dispatch('numbers/increase', { add10: 10 });
     },
   },
   computed: {
